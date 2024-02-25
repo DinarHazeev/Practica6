96f5a75d1280e757505883279479aed3f663f60b
DinarHazeev <dinar.khazeev.04@bk.ru>
2024-02-25 14:59:40 +0300

    Выполнен расчет F

--- Практика 6/Program.cs
+++ Практика 6/Program.cs
@@ -13,6 +13,7 @@ namespace Практика_6
             Console.WriteLine("Введите значение x ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+            double f = 3*Math.Sin(x+2);
         }
     }
 }
